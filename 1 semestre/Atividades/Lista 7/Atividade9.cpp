624761fa698c9020af88bf31a07606109f317258
ArturBM <arturbm05@gmail.com>
2024-06-27 16:36:55 -0300

    Commit algoritimo ordenação

--- 1 semestre/Atividades/Lista 7/Atividade9.cpp
+++ 1 semestre/Atividades/Lista 7/Atividade9.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 main()
 {
-    int i, A[5], Ordem[5];
+    int i, A[5],aux, continua;
 
     for(i=0;i<5;i++)
     {
@@ -9,9 +9,27 @@ main()
         scanf("%d", &A[i]);
 
     }
-
-    for(i=0;i<4;i++)
+continua = 1;
+    while(continua != 0)
     {
-        printf("%d\t", A[5]);
+        continua =0;
+        for(i=0; i< 4;i++)
+        {
+            if(A[i] > A[i + 1])
+            {
+                aux= A[i];
+                A[i] = A[i + 1];
+
+                A[i + 1] = aux;
+                continua = 1;
+            }
+        } 
+      }
+
+         for(i=0;i<5;i++)
+    {
+        printf("%d\t", A[i]);
     }
+     printf("\n");
 }
+       
