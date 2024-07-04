5cd3b5808de958d18a189c279b9bc052f766f514
ArturBM <arturbm05@gmail.com>
2024-07-03 21:19:58 -0300

    commit pereira

---
+++ 1 semestre/Atividades/Lista 7/Atividade16.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int Q[10], CP[10],CL[10];
+
+    for(int i = 0; i<9; i++)
+    {
+            printf("Informe seu codigo de consumidor: ");
+            scanf("%d", &CL[i]);
+    }
+        
+
+
+
+
+}
