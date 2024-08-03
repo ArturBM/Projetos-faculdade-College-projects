94774f2e9a81b241ee73b9dd12d96ca199d83634
Artur Machado <arturbm05@gmail.com>
2024-08-02 22:59:00 -0300

    Aula Faculdade

--- 2 semestre/Laboratório de programação/Atividades/Lista 1/Atividade3.cpp
+++ 2 semestre/Laboratório de programação/Atividades/Lista 1/Atividade3.cpp
@@ -1,7 +1,14 @@
 #include<stdio.h>
 main()
 {
-    
+        float precofab, lucro, imposto, total;
+        
+        printf("Qual o valor do preco de fabrica: ");
+        scanf("%f", &precofab);
+
+    printf("Qual o percentual do distribuidor: ");
+        scanf("%f", &lucro);
+
 
 
 
