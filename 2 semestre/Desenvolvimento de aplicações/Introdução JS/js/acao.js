7d51a95e593321d08afb3c803e146c71ee6fe105
Artur Machado <arturbm05@gmail.com>
2024-10-07 19:15:22 -0300

    commit faculdade

---
+++ 2 semestre/Desenvolvimento de aplicações/Introdução JS/js/acao.js
@@ -0,0 +1,11 @@
+// alert("HelloWorld")
+
+var nome = "Artur"
+var sobrenome = "Machado"
+const dolarHoje= 5.57
+
+var textoB = document.getElementById("textoB")
+
+const liLista = document.querySelectorAll("li")
+
+console.log(liLista[0].innerHTML)
